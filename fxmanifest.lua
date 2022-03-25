2f7d0b874115f04ea2a14bc105d29e34d0a8565d
reylagardo <88545906+reylagardo@users.noreply.github.com>
2022-03-25 22:41:48 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Rey Lagardo'
+description 'Discord : https://discord.io/OMREY'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'music/music.mp3'
+}
